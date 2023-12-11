13631b5e919a9300cc2791b6cecca06d7325012a
0xNafri <irfanfmurad@gmail.com>
2023-12-11 17:43:03 +0800

    added db + props in contactlist page

--- server/mock_data.py
+++ server/mock_data.py
@@ -1,13 +1,13 @@
 # data.py
 contacts = [
-    {"name": "John Doe", "number": "123-456-7890"},
-    {"name": "Jane Doe", "number": "987-654-3210"},
-    {"name": "Alice Smith", "number": "555-123-4567"},
-    {"name": "Bob Johnson", "number": "333-888-5555"},
-    {"name": "Eva Davis", "number": "777-111-2222"},
-    {"name": "Sam Wilson", "number": "999-444-6666"},
-    {"name": "Emily Brown", "number": "111-222-3333"},
-    {"name": "Chris Taylor", "number": "444-999-7777"},
-    {"name": "Olivia White", "number": "666-333-8888"},
-    {"name": "Daniel Lee", "number": "888-555-4444"},
+    {"name": "John Doe", "phone_number": "123-456-7890"},
+    {"name": "Jane Doe", "phone_number": "987-654-3210"},
+    {"name": "Alice Smith", "phone_number": "555-123-4567"},
+    {"name": "Bob Johnson", "phone_number": "333-888-5555"},
+    {"name": "Eva Davis", "phone_number": "777-111-2222"},
+    {"name": "Sam Wilson", "phone_number": "999-444-6666"},
+    {"name": "Emily Brown", "phone_number": "111-222-3333"},
+    {"name": "Chris Taylor", "phone_number": "444-999-7777"},
+    {"name": "Olivia White", "phone_number": "666-333-8888"},
+    {"name": "Daniel Lee", "phone_number": "888-555-4444"},
 ]
--- server/server.py
+++ server/server.py
@@ -1,13 +1,41 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from mock_data import contacts
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///contacts.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=True)
+    phone_number = db.Column(db.String(15), nullable=True)
+
+
 
 # App routes
+@app.route('/add_contact', methods=['POST'])
+def add_contact():
+    data = request.get_json()
+    new_contact = Contact(name=data['name'], phone_number=data['phone_number'])
+    db.session.add(new_contact)
+    db.session.commit()
+    return jsonify({'message': 'Contact added successfully'})
+
+@app.route('/get_contacts', methods=['GET'])
+def get_contacts():
+    contacts = Contact.query.all()
+    contacts_data = [{'id': contact.id, 'name': contact.name, 'phone_number': contact.phone_number} for contact in contacts]
+    return jsonify({'contacts': contacts_data})
+
 @app.route("/data", methods=['GET'])
 def get_contacts():
     return jsonify({"contacts": contacts})
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
